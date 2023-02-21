ee0a0dd2b640229c36f939adc64da2eb73dc38dc
glassonionmovies <glassonionmovies@gmail.com>
2023-02-21 17:34:19 -0500

    Working UI with TFlite on mac

--- tkinter/ui-tflite.py
+++ tkinter/ui-tflite.py
@@ -13,8 +13,14 @@ import numpy as np
 from PIL import Image, ImageTk
 import cv2
 import sys
-
-
+import argparse
+import sys
+import time
+import cv2
+from tflite_support.task import core
+from tflite_support.task import processor
+from tflite_support.task import vision
+import utils
 
 full_screen=True
 
@@ -27,6 +33,9 @@ except:
     full_screen=True
 
 
+maxResults=6
+scoreThreshold=0.5
+cameraId=0
     
     
 import numpy as np
@@ -54,6 +63,92 @@ with open(classesFile, 'rt') as f:
     
 modelWeights = '../MyModels/burger_feb17.onnx'
 net = cv2.dnn.readNet(modelWeights)
+
+
+
+
+
+#run('../MyModels/WT_efficientdet-lite-salad.tflite',cameraId,640,320,4,False)
+
+#def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
+#        enable_edgetpu: bool) -> None:
+   # Variables to calculate FPS
+
+  # Continuously capture images from the camera and run inference
+
+
+model='../MyModels/WT_efficientdet-lite-salad.tflite'
+camera_id=0
+width=640
+height=320
+num_threads=4
+enable_edgetpu=False
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument(
+      '--model',
+      help='Path of the object detection model.',
+      required=False,
+      default='efficientdet_lite0.tflite')
+  parser.add_argument(
+      '--cameraId', help='Id of camera.', required=False, type=int, default=-1)
+  parser.add_argument(
+      '--frameWidth',
+      help='Width of frame to capture from camera.',
+      required=False,
+      type=int,
+      default=640)
+  parser.add_argument(
+      '--frameHeight',
+      help='Height of frame to capture from camera.',
+      required=False,
+      type=int,
+      default=480)
+  parser.add_argument(
+      '--numThreads',
+      help='Number of CPU threads to run the model.',
+      required=False,
+      type=int,
+      default=4)
+  parser.add_argument(
+      '--maxResults',
+      help='Max Results.',
+      required=False,
+      type=int,
+      default=6)
+  parser.add_argument(
+      '--scoreThreshold',
+      help='scoreThreshold.',
+      required=False,
+      type=float,
+      default=0.45)
+  parser.add_argument(
+      '--enableEdgeTPU',
+      help='Whether to run the model on EdgeTPU.',
+      action='store_true',
+      required=False,
+      default=False)
+  args = parser.parse_args()
+    
+  global scoreThreshold
+  scoreThreshold=float(args.scoreThreshold)
+  global maxResults
+  maxResults=int(args.maxResults)
+  global cameraId
+  cameraId=int(args.cameraId)
+
+
+
+
+
+
+
+
+
+
+
 
 def draw_label(im, label, x, y):
     """Draw text onto image at location."""
@@ -348,6 +443,97 @@ root.bind('<Key>',key_pressed)
 
 
 
+main()
+counter, fps = 0, 0
+start_time = time.time()
+
+# Start capturing video input from the camera
+cap = cv2.VideoCapture(camera_id)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+# Visualization parameters
+row_size = 20  # pixels
+left_margin = 24  # pixels
+text_color = (0, 0, 255)  # red
+font_size = 1
+font_thickness = 1
+fps_avg_frame_count = 10
+
+
+base_options = core.BaseOptions(
+  file_name=model, use_coral=enable_edgetpu, num_threads=num_threads)
+detection_options = processor.DetectionOptions(
+  max_results= maxResults, score_threshold=scoreThreshold)
+print(f"maxResults {maxResults}")
+print(f"scoreThreshold {scoreThreshold}")
+options = vision.ObjectDetectorOptions(
+  base_options=base_options, detection_options=detection_options)
+detector = vision.ObjectDetector.create_from_options(options)
+
+
+
+
+
+
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      sys.exit(
+          'ERROR: Unable to read from webcam. Please verify your webcam settings.'
+      )
+
+    counter += 1
+    image = cv2.flip(image, 1)
+
+    # Convert the image from BGR to RGB as required by the TFLite model.
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Create a TensorImage object from the RGB image.
+    input_tensor = vision.TensorImage.create_from_array(rgb_image)
+
+    # Run object detection estimation using the model.
+    detection_result = detector.detect(input_tensor)
+
+    # Draw keypoints and edges on input image
+    image = utils.visualize(image, detection_result)
+
+    # Calculate the FPS
+    if counter % fps_avg_frame_count == 0:
+      end_time = time.time()
+      fps = fps_avg_frame_count / (end_time - start_time)
+      start_time = time.time()
+
+    # Show the FPS
+    fps_text = 'FPS = {:.1f}'.format(fps)
+    text_location = (left_margin, row_size)
+    image=cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                font_size, text_color, font_thickness)
+
+    # Stop the program if the ESC key is pressed.
+    if cv2.waitKey(1) == 27:
+      break
+    #cv2.imshow('object_detector', image)
+
+        #Converting the video for Tkinter
+    detected_frame = cv2.resize(image, (vid_width,vid_height), interpolation = cv2.INTER_AREA)
+    cv2image = cv2.cvtColor(detected_frame, cv2.COLOR_BGR2RGBA)
+    #cv2image = detected_frame#cv2.cvtColor(, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+
+    #Setting the image on the label
+    video.config(image=imgtk)
+    root.update() #Updates the Tkinter window
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
 
 
 
@@ -365,12 +551,9 @@ while(True):
     
     #Converting the video for Tkinter
     detected_frame = cv2.resize(detected_frame, (vid_width,vid_height), interpolation = cv2.INTER_AREA)
-    
     cv2image = cv2.cvtColor(detected_frame, cv2.COLOR_BGR2RGBA)
     #cv2image = detected_frame#cv2.cvtColor(, cv2.COLOR_BGR2RGBA)
-    
     img = Image.fromarray(cv2image)
-
     imgtk = ImageTk.PhotoImage(image=img)
     
     #Setting the image on the label
